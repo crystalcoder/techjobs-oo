fc0a8b9e2af382543c046e9c4256a278d57c7d71
Nick Abbott <Nicholas.Abbott.BSEE@gmail.com>
2017-07-19 17:32:57 -0500

    6 TODOs of assignment completed

--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -28,6 +28,20 @@ public class JobForm {
         Don't forget to add getters and setters
      */
 
+    @NotNull
+    private int locationId;
+
+    @NotNull
+    private int coreCompetencyId;
+
+    @NotNull
+    private int positionTypeId;
+
+    /*
+       TODO #3 - Included other fields needed to create a job,
+       with correct validation attributes and display names.
+       Don't forget to add getters and setters
+    */
     private ArrayList<Employer> employers;
     private ArrayList<Location> locations;
     private ArrayList<CoreCompetency> coreCompetencies;
@@ -38,6 +52,13 @@ public class JobForm {
         JobData jobData = JobData.getInstance();
 
         /*
+            TODO #4 - populate the other ArrayList collections needed in the view
+        */
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
+
+       /*
             TODO #4 - populate the other ArrayList collections needed in the view
         */
 
@@ -59,6 +80,30 @@ public class JobForm {
 
     public void setEmployerId(int employerId) {
         this.employerId = employerId;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getCoreCompetencyId() {
+        return coreCompetencyId;
+    }
+
+    public void setCoreCompetencyId(int coreCompetencyId) {
+        this.coreCompetencyId = coreCompetencyId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
     }
 
     public ArrayList<Employer> getEmployers() {
